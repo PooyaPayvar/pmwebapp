8b44ab31b4cff669107f25767e096077783654e1
PooyaPayvar <pooya.payvar25@gmail.com>
2025-01-08 09:06:29 +0000

    Modify

--- src/pages/TechnicianSubmit.jsx
+++ src/pages/TechnicianSubmit.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import { Link } from "react-router-dom";
 import axios from "axios";
 import { MdOutlineNavigateNext } from "react-icons/md";
 import "./submitstyle.css";
